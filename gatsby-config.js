f60c17cf2aac18c1861ffbb3fa5f6cea27652870
Kamontat Chantrachirathumrong <kamontat.c@hotmail.com>
2018-09-02 15:57:44 +0700

    [config] Set config and library

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,14 @@
+const isProduction = process.env.NODE_ENV === 'production'
+
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Website',
+    version: require('./package.json').version,
   },
   plugins: [
     'gatsby-plugin-netlify-cms',
     'gatsby-plugin-react-helmet',
+    'gatsby-plugin-offline',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -16,7 +20,18 @@ module.exports = {
         display: 'minimal-ui',
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
+    }, {
+      resolve: `gatsby-plugin-emotion`,
+      options: {
+        hoist: true,
+        autoLabel: true,
+        sourceMap: !isProduction,
+      },
+    }, {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        // precision: 8,
+      },
     },
-    'gatsby-plugin-offline',
   ],
 }
