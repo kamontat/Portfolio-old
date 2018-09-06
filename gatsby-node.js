06a02a47d26f8b746fa6ee7a9917f65e58ce8e21
Kamontat Chantrachirathumrong <kamontat.c@hotmail.com>
2018-09-06 23:36:34 +0700

    [refactor] Refactor gatsby node config file

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,14 +4,13 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
+const path = require('path')
+
 // You can delete this file if you're not using it
-exports.modifyWebpackConfig = ({ config, env }) => {
-  config.merge({
+exports.onCreateWebpackConfig = ({ stage, actions }) => {
+  actions.setWebpackConfig({
     resolve: {
-      alias: {
-        '_variables.sass': path.resolve(__dirname, './src/styles/_variables.sass')
-      }
+      modules: [path.resolve(__dirname, "src"), "node_modules"],
     }
   })
-  return config
 }
--- src/_templates/_with_class.style.tsx
+++ src/_templates/_with_class.style.tsx
@@ -3,18 +3,8 @@
 // import { withStyles, createStyles } from '@material-ui/core/styles'
 // import { Theme } from '@material-ui/core'
 
-<<<<<<< HEAD
-// const style = (theme: Theme) => createStyles({
-//   // custom style
-// })
-
-// export default (container: React.ComponentClass) => {
-//   return withStyles(style, { withTheme: true })(container)
-// }
-=======
 // const styles = (theme: Theme) => createStyles({
 //   // custom style
 // })
 
 // export default withStyles(styles, { withTheme: true })
->>>>>>> master
